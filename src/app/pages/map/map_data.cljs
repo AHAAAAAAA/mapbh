e60a8ceac0c429c5011b6421d327d9fcacae42e7
Ahmed Almutawa <ahmed.almutawa@colorado.edu>
2021-02-02 07:55:46 -0700

    Make source/issuer uniform links

--- src/app/pages/map/map_data.cljs
+++ src/app/pages/map/map_data.cljs
@@ -28,8 +28,9 @@
                      :url "https://api.maptiler.com/tiles/1c3695ec-61ea-4e60-be6e-87bf60ceca38/{z}/{x}/{y}.png?key=cAEzKPncCfFpXo43gfxY"
                      :description "Plan of Bahrain. The plan indicates hydrology, settlements and  as well as providing some indication of relief. Included is a table of reference of symbols used. Prepared by naval staff in the Admiralty's Naval Intelligence Division."
                      :notes "Original scan of document was distorted and outlines begin to diverge greatly further and further south."
-                     :source-link "https://www.qdl.qa/en/archive/8955/vdc_90043097172.0x000077"
+                     :issuer-link "https://www.qdl.qa/en/archive/8955/vdc_90043097172.0x000077"
                      :source "British Library: India Office Records and Private Papers"
+                     :source-link "/maps/1937-Bahrain.tif"
                      :issuer "Qatar Digital Library"
                      :scale "1:63,360"
 
@@ -43,8 +44,9 @@
                      :description " Shows physical features, residential places, cultivated land, selected buildings, marsh land, and major roads.
   Relief shown by landform drawings, contours, and spot heights. Compiled from Air photographs in A.H.Q. Drawing Office, Hinaidi, 1937. Drawn and Heliographed by O.S. 1939."
                      :notes "Well-preserved, undistorted scan. Map has relatively few inaccuracies."
-                     :source-link "https://legacy.lib.utexas.edu/maps/ams/bahrein_island/txu-pclmaps-oclc-6559195-bahrein-island.jpg"
+                     :issuer-link "https://legacy.lib.utexas.edu/maps/ams/bahrein_island/txu-pclmaps-oclc-6559195-bahrein-island.jpg"
                      :source "US Army Map Service"
+                     :source-link "/maps/1943-Bahrain.tif"
                      :issuer "University of Texas Library"
                      :scale "1:63,360"
                      :opts (merge base-opts {:maxZoom 20
@@ -56,10 +58,11 @@
                       :url "https://api.maptiler.com/tiles/a77144f2-7690-4c07-8852-3ac78dca0f35/{z}/{x}/{y}.png?key=cAEzKPncCfFpXo43gfxY"
                       :description nil
                       :notes "Accurate in Old Muharraq. Drifts in the southern part of the map near Bu Maher."
-                      :source-link "/maps/1931-Muharraq.jpg"
+                      :issuer-link "/maps/1931-Muharraq.jpg"
                       :source "Unknown"
+                      :source-link "/maps/1931-Muharraq.tif"
                       :scale "1:200ft"
-                      :issuer "Historic Maps of Bahrain 1817 - 1970"
+                      :issuer "Historic Maps of Bahrain 1817 - 1970 - Jarman"
                       :opts (merge base-opts {:maxZoom 20
                                              :minZoom 9
                                              :minNativeZoom 13
@@ -69,10 +72,11 @@
                       :url "https://api.maptiler.com/tiles/7e37e696-7889-461f-81d3-8a3b5e5e0097/{z}/{x}/{y}.png?key=cAEzKPncCfFpXo43gfxY"
                       :description "The map shows Muhurraq Island, and the causeway linking Muharraq to Manama. Also shown are principal roads, tracks, buildings (including the Political Agency in Manana), islands, and the location of several underwater freshwater springs. A rectangle on Muharraq Island indicates the area specified for the landing ground."
                       :notes "Well-preserved and decently accurate."
-                      :source-link "https://www.qdl.qa/en/archive/8955/vdc_9002560898.0x000080"
+                      :source-link "/maps/1937-Muharraq.tif"
                       :source "British Library: India Office Records and Private Papers"
                       :scale "1:20,000"
                       :issuer "Qatar Digital Library"
+                      :issuer-link "https://www.qdl.qa/en/archive/8955/vdc_9002560898.0x000080"
                       :opts (merge base-opts {:maxZoom 20
                                               :minZoom 9
                                               :minNativeZoom 11
@@ -82,10 +86,11 @@
                      :url "https://api.maptiler.com/tiles/56815264-b7fe-4b4f-900d-979cb50ff777/{z}/{x}/{y}.png?key=cAEzKPncCfFpXo43gfxY"
                      :description "Plan of Manamah, Bahrain. The plan indicates settlements, streets, buildings and landmarks. Prepared by naval staff in the Admiralty's Naval Intelligence Division."
                      :notes "Some map drift along the southern and western ends of the document. Mostly accurate."
-                     :source-link "https://www.qdl.qa/en/archive/81055/vdc_100043097172.0x000079"
+                     :source-link "/maps/1937-Manama.tif"
                      :source "British Library: India Office Records and Private Papers"
                      :scale "1:3,639"
                      :issuer "Qatar Digital Library"
+                     :issuer-link "https://www.qdl.qa/en/archive/81055/vdc_100043097172.0x000079"
                      :opts (merge base-opts {:maxZoom 20
                                              :minZoom 9
                                              :minNativeZoom 13
@@ -95,29 +100,30 @@
                      :url "https://api.maptiler.com/tiles/9d3b9e4f-aa2a-4e5e-9301-d1aec153a104/{z}/{x}/{y}.png?key=cAEzKPncCfFpXo43gfxY"
                      :description "Mosaic prepared by SPC RE in 1969 from air photography dated October 1968 and trig control. Names taken from K962 sheet Manama dated 1963. Names and cultural details raised from various sources 1969. This plan supersedes series K962 edit on 3.GSGS sheet Manama."
                      :notes "Excellent."
-                     :source-link "/maps/1969-Manama.jpg"
-
+                     :source-link "/maps/1969-Manama.tif"
                      :source "Ministry of Defence, United Kingdom, 1969"
                      :scale "1:5,000"
-                     :issuer "Historic Maps of Bahrain 1817 - 1970"
+                     :issuer "Historic Maps of Bahrain 1817 - 1970 - Jarman"
+                     :issuer-link "/maps/1969-Manama.jpg"
                      :opts (merge base-opts {:maxZoom 20
                                              :minZoom 9
                                              :minNativeZoom 13
                                              :maxNativeZoom 18
                                              :opacity 0.7})}
    "1977 - MANAMA" {:title "MANAMA"
-                     :url "https://api.maptiler.com/tiles/2db42a49-7739-4ccf-8740-1ed670a5aea0/{z}/{x}/{y}.png?key=cAEzKPncCfFpXo43gfxY"
-                     :description "This map has been compiled and drawn from photography taken during Jan/Feb 1977 by Fairey Surveys LTD., Reform Road, Maidenhead, Berkshire, England. for The State of Bahrain."
-                     :notes "Part of a 7 map series. Covers northeast Bahrain from Muharraq to Bahrain Fort and down to Ras Sanad. Direct translation from UTM Zone 39 to Web Mercator with some raster massaging."
-                     :source-link "/maps/Manama 1977.jpg"
-                     :source "Fairey Surveys Ltd"
-                     :scale "1:25,000"
-                     :issuer nil
-                     :opts (merge base-opts {:maxZoom 20
-                                             :minZoom 9
-                                             :minNativeZoom 11
-                                             :maxNativeZoom 15
-                                             :opacity 0.8})}})
+                    :url "https://api.maptiler.com/tiles/2db42a49-7739-4ccf-8740-1ed670a5aea0/{z}/{x}/{y}.png?key=cAEzKPncCfFpXo43gfxY"
+                    :description "This map has been compiled and drawn from photography taken during Jan/Feb 1977 by Fairey Surveys LTD., Reform Road, Maidenhead, Berkshire, England. for The State of Bahrain."
+                    :notes "Part of a 7 map series. Covers northeast Bahrain from Muharraq to Bahrain Fort and down to Ras Sanad. Direct translation from UTM Zone 39 to Web Mercator with some raster massaging."
+                    :source-link "/maps/1977-Manama.tif"
+                    :source "Fairey Surveys Ltd"
+                    :scale "1:25,000"
+                    :issuer "Unknown"
+                    :issuer-link "/maps/Manama 1977.jpg"
+                    :opts (merge base-opts {:maxZoom 20
+                                            :minZoom 9
+                                            :minNativeZoom 11
+                                            :maxNativeZoom 15
+                                            :opacity 0.8})}})
 
 (def ar-layers
   {"1937 - BAHRAIN" {:title "جزيرة البحرين - ١٩٣٧ "
