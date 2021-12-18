e4f043d084b361645be25019c5f8296d815519dd
Ahmed Almutawa <mutawa.5@gmail.com>
2021-12-18 16:04:48 +0300

    Add server config files

--- scripts/tif2mbtiles.sh
+++ scripts/tif2mbtiles.sh
@@ -3,5 +3,5 @@ set -e
 INPUT=$1
 OUTPUT=$2
 
-gdal_translate "$INPUT" "$OUTPUT" -of MBTILES
-gdaladdo -r average "$OUTPUT" 2 4 8 16 32
+gdal_translate "$INPUT" "$OUTPUT" -if GTiff -of MBTILES
+gdaladdo -r cubic "$OUTPUT" 2 4 8 16 32 64 128 256 512 1024
