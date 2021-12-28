94ab26e976e2102045fb78ba3778fa7a294ee2de
Ahmed Almutawa <mutawa.5@gmail.com>
2021-12-28 23:22:59 +0300

    Update contribute page

--- src/app/pages/contribute.cljs
+++ src/app/pages/contribute.cljs
@@ -3,27 +3,26 @@
 (defn en []
   [:div.container.contribute
    [:h1.title "How to contribute"]
-   [:p "All contributions are welcome! We're currently working on open sourcing the code and dataset as time allows. The current needs primarily revolve adding as many interesting maps as possible. This involves working on:"]
+   [:p "All contributions are welcome! The code and dataset are "
+    [:a {:href "https://github.com/AHAAAAAAA/mapbh"} "open-sourced and freely available."] " The current needs revolve around adding as many interesting maps as possible. This involves:"]
    [:ul {:style {:margin "2% 12%"}}
-    [:li "• Acquistion/Digitization of historic maps."]
+    [:li "• Acquistion/digitization of historic maps."]
     [:li "• Translating old map projections to Web Mercator."]
-    [:li "• Fixing alignment issues on existing maps."]
     [:li "• Creating compelling social media content."]
-    [:li "• Cleanup of artifacts on older maps."]
+    [:li "• Manipulating/correcting images and scans."]
     [:li "• Arabic translation of map metadata.\n"]]
-   [:p "If this interests you or you have any other suggestions or maps to share, reach out to us by " [:a {:href "mailto:mapbh.org@gmail.com"} "email"] ". All experience levels are welcome whether you're an expert or a complete novice. You will be empowered to make changes and/or supported in your learning. General reminder when making requests that this project is done entirely in my free time and funded with my own money, both of which exist in limited supply. Please excuse any slowness to respond."]
+   [:p "If this interests you or you have any other suggestions or maps to share, reach out to us by " [:a {:href "mailto:mapbh.org@gmail.com"} "email"] ". All experience levels are welcome whether you're an expert or a novice. You will be empowered to make changes and/or supported. General reminder when making requests that this project is done entirely in my free time and funded with my own money, both of which exist in limited supply. Please excuse any slowness to respond."]
    [:p "In the information age, we're subjected to an increasing barrage of data that tells fewer stories every day. It's my wish for this one-man project to grow into a team that tells those stories of Bahrain's oft-forgotten history currently tucked away in dusty library shelves and dog-earred journals."]
-   [:p "Special thanks to Abrar Hasan and Mohamed Toorani for their help in sourcing and cataloging these maps, Majd Al-Shihabi, Maryam Zainal, Ali Karimi, Ahmad Zakaria and Ali Al-Jamri for sharing valuable insights and translations, and everyone else that put up with me talking about maps and street intersections way too much."]])
+   [:p "Special thanks to Abrar Hasan and Mohamed Toorani for their help in sourcing and cataloging these maps, Majd Al-Shihabi, Maryam Zainal, Ali Karimi, Ahmad Zakaria and Ali Al-Jamri for sharing valuable insights and translations, and many others that put up with me talking about maps and street intersections way too much."]])
 
 
 (defn ar []
   [:div.container.contribute {:dir "rtl" :lang "ar" :style {:font-size "125%"}}
    [:h1.title "كيفية المساهمة في المشروع"]
-   [:p "نرحب بجميع المساهمات! نعمل حاليًا على نشر مصدر الموقع ومجموعة البيانات بقدر ما يسمح به الوقت. الاحتياجات الحالية تتمحور حول إضافة أكبر عدد ممكن من الخرائط المهمة، هذا يتضمن العمل التالي:"]
+   [:p "نرحب بجميع المساهمات! الموقع ومجموعة البيانات " [:a {:href "https://github.com/AHAAAAAAA/mapbh"} "مفتوح المصدر و مجاني."] " الاحتياجات الحالية تتمحور حول إضافة أكبر عدد ممكن من الخرائط، وهذا يتضمن العمل التالي:"]
    [:ul {:style {:margin "2% 12%"}}
     [:li "• اقتناء وترقيم خرائط تاريخية"]
     [:li "• ترجمة إسقاطات خرائطية قديمة إلى إسقاط «مركاتور» الشبكي"]
-    [:li "• تعديل ومحاذاة الخرائط المتاحة"]
     [:li "• تصميم محتوى لوسائل التواصل الاجتماعي"]
     [:li "• معالجة الخرائط من الآثار المطبعية"]
     [:li "• ترجمة البيانات الوصفية للخرائط للعربية"]]
